78ef663c9e66e2125a3c1c5233a6d43502582da0
Michele Wyss <michele_wyss@students.unibe.ch>
2015-05-25 18:45:30 +0200

    Fixed some other silly mistakes.

--- app/controllers/runs_controller.rb
+++ app/controllers/runs_controller.rb
@@ -69,6 +69,6 @@ class RunsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def run_params
-      params.require(:run).permit(:start, :duration, :runner_id)
+      params.require(:run).permit(:start, :duration, :runner_id, :category_id)
     end
 end
--- app/models/run.rb
+++ app/models/run.rb
@@ -1,4 +1,4 @@
 class Run < ActiveRecord::Base
   belongs_to :runner
-  belongs_to :cagegory
+  belongs_to :category
 end
