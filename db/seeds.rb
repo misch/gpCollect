628cea4a69c6f9451aff6a452f4a3f64468ee1c5
Michele Wyss <michele_wyss@students.unibe.ch>
2015-08-21 11:15:40 +0200

    adapted seed to the changes in be58ee18f0f3bc37877575f0e59c0195ddaab606

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,45 +16,54 @@ end
 Run.delete_all
 Runner.delete_all
 Category.delete_all
+Route.delete_all
+Organizer.delete_all
+RunDay.delete_all
 
-CSV.open('db/data/gp_bern_10m_2015.csv', headers: true, col_sep: ';').each do |line|
-  category_hash = {}
-  runner_hash = {}
-  name = line[4]
-  category_string = line[5]
-  runner_hash[:club_or_hometown] = line[6]
-  begin
-    # E. g. 'Abati, Mauro (SUI)'
-    m = name.match /([^,]*), ([^(]*) \(([A-Z]*)\)/ do |matches|
-      runner_hash[:last_name] = matches[1]
-      runner_hash[:first_name] = matches[2]
-      runner_hash[:nationality] = matches[3]
-    end
-    raise 'Did not match!' unless m
+route = Route.create!(length: 16.093)
+gp_bern_organizer = Organizer.create!(name: "Grand Prix von Bern")
+run_day = RunDay.create!(organizer: gp_bern_organizer, date: Date.new(2015, 5, 9), weather: "dunno", route: route)
 
-    unless category_string.blank?
-      category_string.match /([MW])U?(\d{1,3})/ do |matches|
-        category_hash[:sex] = matches[1]
-        if category_string[1] == 'U'
-          category_hash[:age_max] = matches[2].to_i
-        else
-          category_hash[:age_min] = matches[2].to_i
+ActiveRecord::Base.transaction do
+  CSV.open('db/data/gp_bern_10m_2015.csv', headers: true, col_sep: ';').each do |line|
+    category_hash = {}
+    runner_hash = {}
+    name = line[4]
+    category_string = line[5]
+    runner_hash[:club_or_hometown] = line[6]
+    begin
+      # E. g. 'Abati, Mauro (SUI)'
+      m = name.match /([^,]*), ([^(]*) \(([A-Z]*)\)/ do |matches|
+        runner_hash[:last_name] = matches[1]
+        runner_hash[:first_name] = matches[2]
+        runner_hash[:nationality] = matches[3]
+      end
+      raise 'Did not match!' unless m
+
+      unless category_string.blank?
+        category_string.match /([MW])U?(\d{1,3})/ do |matches|
+          category_hash[:sex] = matches[1]
+          if category_string[1] == 'U'
+            category_hash[:age_max] = matches[2].to_i
+          else
+            category_hash[:age_min] = matches[2].to_i
+          end
         end
       end
+    rescue Exception => e
+      puts "Failed parsing: #{line}"
+      raise e
     end
-  rescue Exception => e
-    puts "Failed parsing: #{line}"
-    raise e
-  end
 
-  category = category_hash.blank? ? nil : Category.find_or_create_by!(category_hash)
-  runner_hash[:sex] = category_hash[:sex]
-  runner = Runner.find_or_create_by!(runner_hash)
-  # TODO: Somehow handle this over multiple years (allow change of hometown)
-  #runner.update_attributes!(club_or_hometown: club_or_hometown)
-  duration_string = line[10]
-  # TODO: think of something to handle intermediary times.
-  unless duration_string.blank?
-    Run.create!(runner: runner, category: category, duration: duration_string_to_milliseconds(duration_string))
+    category = category_hash.blank? ? nil : Category.find_or_create_by!(category_hash)
+    runner_hash[:sex] = category_hash[:sex]
+    runner = Runner.find_or_create_by!(runner_hash)
+    # TODO: Somehow handle this over multiple years (allow change of hometown)
+    #runner.update_attributes!(club_or_hometown: club_or_hometown)
+    duration_string = line[10]
+    # TODO: think of something to handle intermediary times.
+    unless duration_string.blank?
+      Run.create!(runner: runner, category: category, duration: duration_string_to_milliseconds(duration_string), run_day: run_day)
+    end
   end
 end
