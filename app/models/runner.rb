ee02e2e7cc0b06eb5ad655a3be24b300fb295e87
panmari <smmuzi@gmail.com>
2015-08-26 20:12:16 +0200

    Added default order for runs.

--- app/models/runner.rb
+++ app/models/runner.rb
@@ -1,5 +1,6 @@
 class Runner < ActiveRecord::Base
-  has_many :runs
+  # TODO: use run_day.date for ordering, seems to be tough to do since it needs another join.
+  has_many :runs, -> { order(run_day_id: :asc) }
   has_many :categories, through: :runs
   has_many :run_days, through: :runs
 
