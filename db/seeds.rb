fba7141e22e894fbf1de457fbcbc860e27a9064f
panmari <smmuzi@gmail.com>
2015-08-27 20:06:56 +0200

    Seeding also older years.
    And using now find_or_create_by! for creating run day for easier
    restarting of parsing.

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,21 +11,25 @@ end
 route_16km = Route.find_or_create_by!(length: 16.093)
 gp_bern_organizer = Organizer.find_or_create_by!(name: 'Grand Prix von Bern')
 
-files = [
+files = (1999..2008).map {|year| {file: "db/data/gp_bern_10m_#{year}.csv",
+                                  run_day: RunDay.find_or_create_by!(organizer: gp_bern_organizer,
+                                                                     date: Date.new(year),
+                                                                     route: route_16km)} } +
+    [
     {file: "db/data/gp_bern_10m_2009.csv", shift: -1,
-     run_day: RunDay.create!(organizer: gp_bern_organizer, date: Date.new(2009, 4, 18), route: route_16km)},
+     run_day: RunDay.find_or_create_by!(organizer: gp_bern_organizer, date: Date.new(2009, 4, 18), route: route_16km)},
     {file: "db/data/gp_bern_10m_2010.csv", shift: -1,
-     run_day: RunDay.create!(organizer: gp_bern_organizer, date: Date.new(2010, 5, 22), route: route_16km)},
+     run_day: RunDay.find_or_create_by!(organizer: gp_bern_organizer, date: Date.new(2010, 5, 22), route: route_16km)},
     {file: "db/data/gp_bern_10m_2011.csv", shift: -1,
-     run_day: RunDay.create!(organizer: gp_bern_organizer, date: Date.new(2011, 5, 14), route: route_16km)},
+     run_day: RunDay.find_or_create_by!(organizer: gp_bern_organizer, date: Date.new(2011, 5, 14), route: route_16km)},
     {file: "db/data/gp_bern_10m_2012.csv", shift: -1,
-     run_day: RunDay.create!(organizer: gp_bern_organizer, date: Date.new(2012, 5, 12), route: route_16km)},
+     run_day: RunDay.find_or_create_by!(organizer: gp_bern_organizer, date: Date.new(2012, 5, 12), route: route_16km)},
     {file: "db/data/gp_bern_10m_2013.csv",
-     run_day: RunDay.create!(organizer: gp_bern_organizer, date: Date.new(2013, 5, 18), route: route_16km)},
+     run_day: RunDay.find_or_create_by!(organizer: gp_bern_organizer, date: Date.new(2013, 5, 18), route: route_16km)},
     {file: "db/data/gp_bern_10m_2014.csv",
-     run_day: RunDay.create!(organizer: gp_bern_organizer, date: Date.new(2014, 5, 10), route: route_16km)},
+     run_day: RunDay.find_or_create_by!(organizer: gp_bern_organizer, date: Date.new(2014, 5, 10), route: route_16km)},
     {file: "db/data/gp_bern_10m_2015.csv",
-     run_day: RunDay.create!(organizer: gp_bern_organizer, date: Date.new(2015, 5, 9), route: route_16km)}
+     run_day: RunDay.find_or_create_by!(organizer: gp_bern_organizer, date: Date.new(2015, 5, 9), route: route_16km)}
 ]
 files.each { |file| SeedHelpers::seed_runs_file file }
 
