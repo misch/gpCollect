dd39fece48af6bd718ee21435e8f5ba211efeb25
panmari <smmuzi@gmail.com>
2015-08-27 11:58:40 +0200

    More complex way of finding GP only rows.

--- lib/tasks/scrape_helpers.rb
+++ lib/tasks/scrape_helpers.rb
@@ -6,8 +6,7 @@ module ScrapeHelpers
   # ["M35", "783. Ayrom Houman", "62 Morges", "1:18.32,7", "8036)", "GM/4146."]  # Target format is
   # Platz;Pl.AK;Pl.(M/W);Nr.;Name;AK;Verein/Ort;Rel *;5km;10km;Zielzeit
   def self.old_html_row_to_csv_row(row)
-    is_gp = row[5].first == 'G'
-    return nil unless is_gp
+    return nil unless is_gp?
     category = if row[0].include? '/'
                  # for year 2000+, this column looks like this: 'GP/M30'
                  row[0].split('/')[1]
@@ -28,5 +27,14 @@ module ScrapeHelpers
 
     rank = RUN_TYPE_OVERALL_RANK_REGEXP.match(row[5])[:rank]
     [rank, rank_category, nil, start_number, csv_name, category, club_or_hometown, nil, nil, nil, time, birth_year]
+  end
+
+  def self.is_gp?(row)
+    if row.size > 6
+      # Is a more modern page, use first attribute to
+      row[0][0..1] == 'GP'
+    else
+      row[5].first == 'G'
+    end
   end
 end
