d03c7d1d04d6d15bf41b159c11ac4a9a78f9d084
panmari <smmuzi@gmail.com>
2015-08-28 18:57:58 +0200

    Displaying mean of category if only showing one runner.

--- app/models/run.rb
+++ app/models/run.rb
@@ -2,4 +2,8 @@ class Run < ActiveRecord::Base
   belongs_to :runner, counter_cache: true
   belongs_to :category
   belongs_to :run_day
+
+  def mean_duration_for_run_day_category
+    Run.where(category_id: category_id, run_day_id: run_day_id).average(:duration)
+  end
 end
