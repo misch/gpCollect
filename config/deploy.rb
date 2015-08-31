9ca313c6faec2fb48790570394ba08e96d0643e4
panmari <smmuzi@gmail.com>
2015-08-31 14:12:47 +0200

    Some additional changes for local asset compilation.
    Asset compilation on the server is veeery slow in our case (raspberry
    pi).

--- config/deploy.rb
+++ config/deploy.rb
@@ -20,7 +20,7 @@ set :deploy_to, '/opt/webapps/gpCollect'
 # set :format, :pretty
 
 # Default value for :log_level is :debug
-# set :log_level, :debug
+set :log_level, :info
 
 # Default value for :pty is false
 # set :pty, true
@@ -29,7 +29,8 @@ set :deploy_to, '/opt/webapps/gpCollect'
 set :linked_files, fetch(:linked_files, []).push('.env')
 
 # Default value for linked_dirs is []
-# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+set :linked_dirs, fetch(:linked_dirs, []).push('public/assets')
+# .push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
@@ -42,25 +43,24 @@ namespace :deploy do
   #Rake::Task["deploy:compile_assets"].clear
 
   desc "Precompile assets locally and then rsync to web servers"
-  task :compile_assets do
-    on roles(:web) do
-      rsync_host = host.to_s
-
+  task :compile_assets_locally do
+    on roles(:app) do |host|
+      execute "mkdir -p #{shared_path}/public/"
       run_locally do
         with rails_env: :production do ## Set your env accordingly.
-          execute :bundle, "exec rake assets:precompile"
+          execute  "bundle exec rake assets:precompile"
         end
-        execute "rsync -av --delete ./public/assets/ #{fetch(:user)}@#{rsync_host}:#{shared_path}/public/assets/"
+        execute "rsync -av --delete ./public/assets/ #{host.user}@#{host}:#{shared_path}/public/assets/"
         execute "rm -rf public/assets"
         # execute "rm -rf tmp/cache/assets" # in case you are not seeing changes
       end
     end
   end
 
-  after :publishing, :do_stuff do
+  after :finished, :compile_assets_and_restart do
     on roles(:all) do
-      'deploy:compile_assets'
-      'service:thin:restart'
+      deploy.compile_assets_locally
+      service.thin.restart
       within release_path do
         execute :rake, 'tmp:clear'
       end
