afe575dab23343a984fb37b3887a34217ff72975
panmari <smmuzi@gmail.com>
2015-09-05 23:07:28 +0200

    Added some validations for merge runner requests.

--- app/models/merge_runners_request.rb
+++ app/models/merge_runners_request.rb
@@ -3,11 +3,18 @@ class MergeRunnersRequest < ActiveRecord::Base
   has_many :runs, through: :runners
 
   INHERITED_ATTRIBUTES = [:first_name, :last_name, :nationality, :sex, :club_or_hometown, :birth_date]
+
+  validates_presence_of *INHERITED_ATTRIBUTES.map { |attr| "merged_#{attr}" }
+  # TODO: Add validation for non-intersecting run days
+  # TODO: Add possibly validation for edit-distance between merged and inherited attribute name.
+  validates :merged_nationality, format: /\A[A-Z]{3}\z/
+  validates :merged_sex, inclusion: { in: %w(M W) }
+
   def self.new_from(merge_candidates)
     # Select most runner with most recent run as default for attributes of merge requests.
-    best_mc = merge_candidates.max_by {|mc| mc.run_days.max_by(&:date) }
+    best_mc = merge_candidates.max_by { |mc| mc.run_days.max_by(&:date) }
     # TODO: possibly do something more sophisticated with birth_date.
-    merge_request_defaults = INHERITED_ATTRIBUTES.each_with_object({}) {|attr, hash| hash["merged_#{attr}"] = best_mc[attr]}
+    merge_request_defaults = INHERITED_ATTRIBUTES.each_with_object({}) { |attr, hash| hash["merged_#{attr}"] = best_mc[attr] }
     merge_request_defaults[:runners] = merge_candidates
     self.new(merge_request_defaults)
   end
@@ -15,7 +22,7 @@ class MergeRunnersRequest < ActiveRecord::Base
   # Instantiates a new runner with data from this merge request and associates all runs with the new instance. Still
   # needs to be saved in order to be written to DB!
   def to_new_runner
-    runner_attributes = INHERITED_ATTRIBUTES.each_with_object({}) {|attr, hash| hash[attr] = self["merged_#{attr}"]}
+    runner_attributes = INHERITED_ATTRIBUTES.each_with_object({}) { |attr, hash| hash[attr] = self["merged_#{attr}"] }
     runner_attributes[:runs] = runs
     Runner.new(runner_attributes)
   end
