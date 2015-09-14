ea091b57dc302939dd33986f7dfba8c55022ed8b
panmari <smmuzi@gmail.com>
2015-09-14 17:43:38 +0200

    Showing stack mini profiler for logged in admins.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action do
+    # If logged in, show rack profiler stats.
+    if admin_signed_in?
+      Rack::MiniProfiler.authorize_request
+    end
+  end
 end
