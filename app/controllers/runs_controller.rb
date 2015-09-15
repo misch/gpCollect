bb8d700e0156eac99086d667918b7a7ad143fe8c
panmari <smmuzi@gmail.com>
2015-09-15 10:02:17 +0200

    Fixed tests, removed lots of actions/views that were not used.
    Removed edit/update/create actions for most models.

--- app/controllers/runs_controller.rb
+++ app/controllers/runs_controller.rb
@@ -1,5 +1,5 @@
 class RunsController < ApplicationController
-  before_action :set_run, only: [:show, :edit, :update, :destroy]
+  before_action :set_run, only: [:show]
 
   # GET /runs
   # GET /runs.json
@@ -10,55 +10,6 @@ class RunsController < ApplicationController
   # GET /runs/1
   # GET /runs/1.json
   def show
-  end
-
-  # GET /runs/new
-  def new
-    @run = Run.new
-  end
-
-  # GET /runs/1/edit
-  def edit
-  end
-
-  # POST /runs
-  # POST /runs.json
-  def create
-    @run = Run.new(run_params)
-
-    respond_to do |format|
-      if @run.save
-        format.html { redirect_to @run, notice: 'Run was successfully created.' }
-        format.json { render :show, status: :created, location: @run }
-      else
-        format.html { render :new }
-        format.json { render json: @run.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /runs/1
-  # PATCH/PUT /runs/1.json
-  def update
-    respond_to do |format|
-      if @run.update(run_params)
-        format.html { redirect_to @run, notice: 'Run was successfully updated.' }
-        format.json { render :show, status: :ok, location: @run }
-      else
-        format.html { render :edit }
-        format.json { render json: @run.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /runs/1
-  # DELETE /runs/1.json
-  def destroy
-    @run.destroy
-    respond_to do |format|
-      format.html { redirect_to runs_url, notice: 'Run was successfully destroyed.' }
-      format.json { head :no_content }
-    end
   end
 
   private
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,33 +1,45 @@
-factory :category do
-  sex 'M'
-  age_min 30
-end
+FactoryGirl.define do
+  factory :category do
+    sex 'M'
+    age_min 30
+  end
 
-factory :run do
-  duration { Faker::Number.between(4618000, 5366200) }
-  category
-end
+  factory :run_day do
+    date { 1.year.ago }
+  end
 
-factory :runner do
-  first_name { Faker::Name.first_name }
-  last_name { Faker::Name.last_name }
-  club_or_hometown { Faker::Address.city }
+  factory :run do
+    duration { Faker::Number.between(4618000, 5366200) }
+    category
+    run_day
+    runner
+  end
 
-  # user_with_posts will create post data after the user has been created
-  factory :runner_with_runs do
-    # posts_count is declared as a transient attribute and available in
-    # attributes on the factory, as well as the callback via the evaluator
-    transient do
-      runs_count 3
-    end
+  factory :runner do
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+    club_or_hometown { Faker::Address.city }
+    birth_date { Faker::Date.between(50.years.ago, 20.years.ago) }
+    # user_with_posts will create post data after the user has been created
+    factory :runner_with_runs do
+      # posts_count is declared as a transient attribute and available in
+      # attributes on the factory, as well as the callback via the evaluator
+      transient do
+        runs_count 3
+      end
 
-    # the after(:create) yields two values; the user instance itself and the
-    # evaluator, which stores all values from the factory, including transient
-    # attributes; `create_list`'s second argument is the number of records
-    # to create and we make sure the user is associated properly to the post
-    after(:create) do |runner, evaluator|
-      create_list(:run, evaluator.runs_count, runner: runner)
+      # the after(:create) yields two values; the user instance itself and the
+      # evaluator, which stores all values from the factory, including transient
+      # attributes; `create_list`'s second argument is the number of records
+      # to create and we make sure the user is associated properly to the post
+      after(:create) do |runner, evaluator|
+        create_list(:run, evaluator.runs_count, runner: runner)
+      end
     end
   end
 
+  factory :admin do
+    email 'test@tester.com'
+    password 'test1234'
+  end
 end
