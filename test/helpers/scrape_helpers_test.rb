f317c0940bb2f621ad1ac5867c7590b4505cc059
panmari <smmuzi@gmail.com>
2015-09-15 14:32:39 +0200

    Rewrote rspec tests to ActionController::TestCase.

--- test/helpers/scrape_helpers_test.rb
+++ test/helpers/scrape_helpers_test.rb
@@ -1,61 +1,64 @@
-require 'rails_helper'
+require 'test_helper'
+require "rspec/expectations"
 require_relative '../../lib/tasks/scrape_helpers'
 
-RSpec.describe 'scrape_helpers' do
-  it 'should match the most common variant with the name location regexp ' do
+class ScrapeHelpersTest < ActionController::TestCase
+  include RSpec::Matchers
+
+  test 'should match the most common variant with the name location regexp ' do
     matches = ScrapeHelpers::NAME_LOCATION_REGEXP.match '20. Volkmer Margrit'
     expect(matches[:rank_category]).to eq('20')
     expect(matches[:name]).to eq('Volkmer Margrit')
     expect(matches[:location]).to be_nil
   end
 
-  it 'should match the variant with location with the name location regexp' do
+  test 'should match the variant with location with the name location regexp' do
     matches = ScrapeHelpers::NAME_LOCATION_REGEXP.match '2. Voegeli Stephan, Bolligen'
     expect(matches[:rank_category]).to eq('2')
     expect(matches[:name]).to eq('Voegeli Stephan')
     expect(matches[:location]).to eq('Bolligen')
   end
 
-  it 'should match the variant with last names with spaces with the name location regexp' do
+  test 'should match the variant with last names with spaces with the name location regexp' do
     matches = ScrapeHelpers::NAME_LOCATION_REGEXP.match '1171. Van Der Lingen Patrick'
     expect(matches[:rank_category]).to eq('1171')
     expect(matches[:name]).to eq('Van Der Lingen Patrick')
     expect(matches[:location]).to be_nil
   end
 
-  it 'should match the variant with last names with spaces and location with the name location regexp' do
+  test 'should match the variant with last names with spaces and location with the name location regexp' do
     matches = ScrapeHelpers::NAME_LOCATION_REGEXP.match '1171. Van Der Lingen Patrick, Bern'
     expect(matches[:rank_category]).to eq('1171')
     expect(matches[:name]).to eq('Van Der Lingen Patrick')
     expect(matches[:location]).to eq('Bern')
   end
 
-  it 'should match the variant with canton  with the name location regexp' do
+  test 'should match the variant with canton  with the name location regexp' do
     matches = ScrapeHelpers::NAME_LOCATION_REGEXP.match '132. Aebi Eugen, Lengnau BE'
     expect(matches[:rank_category]).to eq('132')
     expect(matches[:name]).to eq('Aebi Eugen')
     expect(matches[:location]).to eq('Lengnau BE')
   end
 
-  it 'should match the variant with large string  with the name location regexp' do
+  test 'should match the variant with large string  with the name location regexp' do
     matches = ScrapeHelpers::NAME_LOCATION_REGEXP.match '1098. Aeschlimann Toni, Ostermundige 58 Ascom 4'
     expect(matches[:rank_category]).to eq('1098')
     expect(matches[:name]).to eq('Aeschlimann Toni')
     expect(matches[:location]).to eq('Ostermundige 58 Ascom 4')
   end
 
-  it 'should match with the run type regexp' do
+  test 'should match with the run type regexp' do
     matches = ScrapeHelpers::RUN_TYPE_OVERALL_RANK_REGEXP.match('GF/361.')
     expect(matches).to_not be_nil
   end
 
-  it 'should split names with two strings into last_name and first_name' do
+  test 'should spltest names with two strings into last_name and first_name' do
     last_name, first_name = ScrapeHelpers::split_name('Meier Helen')
     expect(first_name).to eq('Helen')
     expect(last_name).to eq('Meier')
   end
 
-  it 'should split names with last_name = "van/von der"  ' do
+  test 'should spltest names with last_name = "van/von der"  ' do
     last_name, first_name = ScrapeHelpers::split_name('von der Heide Helen')
     expect(first_name).to eq('Helen')
     expect(last_name).to eq('von der Heide')
@@ -65,7 +68,7 @@ RSpec.describe 'scrape_helpers' do
     expect(last_name).to eq('Van der Sluis')
   end
 
-  it 'should split names with last_name = "van/von/di/de/... "  ' do
+  test 'should spltest names with last_name = "van/von/di/de/... "  ' do
     last_name, first_name = ScrapeHelpers::split_name('van Empden Carsten')
     expect(first_name).to eq('Carsten')
     expect(last_name).to eq('van Empden')
@@ -75,9 +78,9 @@ RSpec.describe 'scrape_helpers' do
     expect(last_name).to eq('von Niederhäusern')
   end
 
-  it 'should split names with more than one last_name and/or first_name in the middle  ' do
+  test 'should spltest names with more than one last_name and/or first_name in the middle  ' do
     last_name, first_name = ScrapeHelpers::split_name('Aridi Rudolf Ameline')
-    expect(first_name).to eq('Rudolf Ameline') # in this case, it doesn't work correcty
+    expect(first_name).to eq('Rudolf Ameline') # in this case, test doesn't work correcty
     expect(last_name).to eq('Aridi')
 
     last_name, first_name = ScrapeHelpers::split_name('Alder Pascal Janik')
@@ -85,7 +88,7 @@ RSpec.describe 'scrape_helpers' do
     expect(last_name).to eq('Alder')
   end
 
-  it 'should split names with van der only if there is still a first name left.' do
+  test 'should spltest names with van der only if there is still a first name left.' do
     last_name, first_name = ScrapeHelpers::split_name('Van Der Lee')
     expect(first_name).to eq('Lee')
     expect(last_name).to eq('Van Der')
--- test/helpers/seed_helpers_test.rb
+++ test/helpers/seed_helpers_test.rb
@@ -1,36 +1,39 @@
-require 'rails_helper'
+require 'test_helper'
+require "rspec/expectations"
 require_relative '../../db/seed_helpers'
 
-RSpec.describe 'seed_helpers' do
-  it 'should match the most common variant with the name regexp' do
+class SeedHelpersTest < ActionController::TestCase
+  include RSpec::Matchers
+  
+  test 'should match the most common variant with the name regexp' do
     matches = SeedHelpers::NAME_REGEXP.match 'Abati, Mauro (SUI)'
     expect(matches[:last_name]).to eq('Abati')
     expect(matches[:first_name]).to eq('Mauro')
     expect(matches[:nationality]).to eq('SUI')
   end
 
-  it 'should match names with composed first names separated by dash with the name regexp' do
+  test 'should match names with composed first names separated by dash with the name regexp' do
     matches = SeedHelpers::NAME_REGEXP.match 'Abegglen, Marie-Thérèse (SUI)'
     expect(matches[:last_name]).to eq('Abegglen')
     expect(matches[:first_name]).to eq('Marie-Thérèse')
     expect(matches[:nationality]).to eq('SUI')
   end
 
-  it 'should match names with composed last names separated by dash with the name regexp' do
+  test 'should match names with composed last names separated by dash with the name regexp' do
     matches = SeedHelpers::NAME_REGEXP.match 'Aegerter-Rüegsegger, Verena (SUI)'
     expect(matches[:last_name]).to eq('Aegerter-Rüegsegger')
     expect(matches[:first_name]).to eq('Verena')
     expect(matches[:nationality]).to eq('SUI')
   end
 
-  it 'should match names with composed first names separated by space with the name regexp' do
+  test 'should match names with composed first names separated by space with the name regexp' do
     matches = SeedHelpers::NAME_REGEXP.match 'Aeschlimann, Karin Andrea (SUI)'
     expect(matches[:last_name]).to eq('Aeschlimann')
     expect(matches[:first_name]).to eq('Karin Andrea')
     expect(matches[:nationality]).to eq('SUI')
   end
 
-  it 'should match names without nationality' do
+  test 'should match names without nationality' do
     matches = SeedHelpers::NAME_REGEXP.match 'von Allmen-Sarmiento, Teresita'
     expect(matches).to be_truthy
     expect(matches[:last_name]).to eq('von Allmen-Sarmiento')
@@ -38,27 +41,27 @@ RSpec.describe 'seed_helpers' do
     expect(matches[:nationality]).to be nil
   end
 
-  it 'should not match if only nationality is present' do
+  test 'should not match if only nationality is present' do
     matches = SeedHelpers::NAME_REGEXP.match '(SUI)'
     expect(matches).to be_falsey
   end
 
-  it 'should turn a duration with dot and all values correctly to miliseconds' do
+  test 'should turn a duration with dot and all values correctly to miliseconds' do
     duration = SeedHelpers::duration_string_to_milliseconds '01:01:01.2'
     expect(duration).to be((1 * 3600 + 1 * 60 + 1) * 1000 + 2 * 100)
   end
 
-  it 'should turn a duration with hours correctly to miliseconds' do
+  test 'should turn a duration with hours correctly to miliseconds' do
     duration = SeedHelpers::duration_string_to_milliseconds '01:00:00,0'
     expect(duration).to be(3600 * 1000)
   end
 
-  it 'should turn a duration without hours correctly to miliseconds' do
+  test 'should turn a duration without hours correctly to miliseconds' do
     duration = SeedHelpers::duration_string_to_milliseconds '50:00,0'
     expect(duration).to be(50 * 60 * 1000)
   end
 
-  it 'should turn a duration without hundreds miliseconds correctly to miliseconds' do
+  test 'should turn a duration without hundreds miliseconds correctly to miliseconds' do
     duration = SeedHelpers::duration_string_to_milliseconds '1:14:24'
     expect(duration).to be(((1 * 3600 + 14 * 60 + 24) * 1000).to_i)
   end
